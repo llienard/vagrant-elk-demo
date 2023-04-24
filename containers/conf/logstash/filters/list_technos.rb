1fc30bf7329caf927e5b792314d58c6d58d524e0
Léo Liénard <leo.lienard@gmail.com>
2023-04-24 09:43:09 +0200

    feat: improve dashboard

--- containers/conf/logstash/filters/list_technos.rb
+++ containers/conf/logstash/filters/list_technos.rb
@@ -4,7 +4,7 @@ def register(params)
 	@source_field = params["source"]
 	@target_field = params["target"]
 	@delimiter = params["delimiter"] != nil ? params["delimiter"] : ","
-
+	@default_version = params["defaultVersion"] != nil ? params["defaultVersion"] : "?"
 end
 
 # the filter method receives an event and must return a list of events.
@@ -16,16 +16,13 @@ def filter(event)
     value = event.get(@source_field)
 
 	if value
-		result = []
+		result = {:list => []}
 		values = value.split(',', -1).map { |v| v.strip  }
 		values.each { |techno|
-			o = {}
 			v = techno.split(':', -1)
-			o[:name] = v.at(0).strip 
-			if v.size > 1
-				o[:version] = v.at(1).strip 
-			end
-			result.append(o)
+			name = v.at(0).strip 
+			result[:list].append(name)
+			result[name] = v.size > 1 ? v.at(1).strip : @default_version != nil ? @default_version : nil
 	 	}
 		event.set(@target_field, result)
 	end
@@ -54,26 +51,29 @@ test "extract multiple languages with versions" do
 		events.size == 1
 	end
 
-	expect("languages is an array") do |events|
-		events.first.get('languages') != nil && events.first.get('languages').kind_of?(Array)
+	expect("languages is an object") do |events|
+		events.first.get('languages') != nil && events.first.get('languages').kind_of?(Object)
+	end
+
+	expect("languages.list is an array") do |events|
+		events.first.get('languages')['list'] && events.first.get('languages')['list'].kind_of?(Object)
 	end
 
 	expect("2 languages extracted") do |events|
-		events.first.get("languages").size == 4
+		events.first.get('languages')['list'].size == 4
 	end
 
 	expect("languages are well filled") do |events|
-		events.first.get("languages").at(0)['name'] == "JAVA"
-		events.first.get("languages").at(0)['version'] == "8"
+		events.first.get('languages')['list'].include? "JAVA"
+		events.first.get("languages")['JAVA'] == "8"
 
-		events.first.get("languages").at(1)['name'] == "JS"
-		events.first.get("languages").at(1)['version'] == "6"
-
-		events.first.get("languages").at(2)['name'] == "jQuery"
-		# events.first.get("languages").at(2)['version'] == "6"
-
-		events.first.get("languages").at(3)['name'] == "NodeJS"
-		events.first.get("languages").at(3)['version'] == "16"
+		events.first.get('languages')['list'].include? "JS"
+		events.first.get("languages")['JS'] == "6"
+		
+		events.first.get('languages')['list'].include? "jQuery"
+		
+		events.first.get('languages')['list'].include? "NodeJS"
+		events.first.get("languages")['NodeJS'] == "16"
 	end
 
 end
@@ -94,21 +94,25 @@ test "extract single language with version" do
 		}
  	}
 
-	expect("1 event sent") do |events|
+	 expect("1 event sent") do |events|
 		events.size == 1
 	end
 
-	expect("languages is an array") do |events|
-		events.first.get('languages') != nil && events.first.get('languages').kind_of?(Array)
+	expect("languages is an object") do |events|
+		events.first.get('languages') != nil && events.first.get('languages').kind_of?(Object)
 	end
 
-	expect("1 language extracted") do |events|
-		events.first.get("languages").size == 1
+	expect("languages.list is an array") do |events|
+		events.first.get('languages')['list'] && events.first.get('languages')['list'].kind_of?(Object)
 	end
 
-	expect("JAVA 8 in languages") do |events|
-		events.first.get("languages").at(0)['name'] == "JAVA"
-		events.first.get("languages").at(0)['version'] == "8"
+	expect("2 languages extracted") do |events|
+		events.first.get('languages')['list'].size == 1
+	end
+
+	expect("languages are well filled") do |events|
+		events.first.get('languages')['list'].include? "JAVA"
+		events.first.get("languages")['JAVA'] == "8"
 	end
 
 end
